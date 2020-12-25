cab33d257b77515311f77294afabe67b8ccea9eb
Joes-BitGit <josephnalmeida@gmail.com>
2020-12-25 02:12:06 -0800

    Working with props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Body = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {props.textDisplay}
+
+      </div>
+      <div>
+        {props.text2}
+
+      </div>
+      <div>
+        {props.addFunc(1, 2)}
+      </div>
     </div>
+
   );
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.props.theFunc(11, 6)}
+          </p>
+          <p>
+            {this.props.title}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.props.num}
+          </a>
+
+        </header>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  add(a, b) {
+    return a + b;
+  }
+  render() {
+    return (
+      <div className="App" >
+        <Header title='render' num={5} myObj={{ a: 5, b: 6 }} theFunc={this.add} />
+
+        <Body textDisplay='i am a programmer' text2='the duality of man' addFunc={this.add} />
+        <Body textDisplay='hackermans' text2='yinyang' addFunc={this.add} />
+        <Body textDisplay='software engineer' text2='reasoned choice' addFunc={this.add} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
